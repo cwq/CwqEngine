51743e55bdf3f9b53a2833fd3ed431e2a4284c23
cwq <common.cwq@gmail.com>
2015-02-13 14:11:21 +0800

    open neon

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,6 +2,12 @@ LOCAL_PATH:=$(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+    LOCAL_CFLAGS += -DHAVE_NEON=1
+    LOCAL_ARM_NEON  := true 
+    CFLAGS += -mfpu=neon 
+endif
+
 LOCAL_MODULE        := cwqengine
 
 LOCAL_SRC_FILES     := CwqEngineJNI.cpp \
