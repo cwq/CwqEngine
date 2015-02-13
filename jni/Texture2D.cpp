f48392908a478616a3bc97c952b944ca5759f595
cwq <common.cwq@gmail.com>
2015-02-13 10:28:41 +0800

    use glTexSubImage2D for same w,h,format image

--- jni/Texture2D.cpp
+++ jni/Texture2D.cpp
@@ -29,25 +29,36 @@ bool Textur2D::load(const unsigned char* fileData, size_t dataLen)
 
 bool Textur2D::load(Image image)
 {
-    GLuint textures[1];
-    glGenTextures(1, textures);
-    if(textures[0] != 0 )
+    if(mWidth != image.getWidth() || mHeight != image.getHeight()
+            || mTextureFormat != image.getFormat())
     {
-        mTextureFormat = image.getFormat();
-        setWidthAndHeight(image.getWidth(), image.getHeight());
-        glBindTexture(GL_TEXTURE_2D, textures[0]);
-        glTexImage2D(GL_TEXTURE_2D, 0, mTextureFormat, mWidth, mHeight, 0, mTextureFormat, GL_UNSIGNED_BYTE, image.getPixels());
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-        mTextureID = textures[0];
-        return true;
+        unLoad();
+        GLuint textures[1];
+        glGenTextures(1, textures);
+        if(textures[0] != 0 )
+        {
+            mTextureFormat = image.getFormat();
+            setWidthAndHeight(image.getWidth(), image.getHeight());
+            glBindTexture(GL_TEXTURE_2D, textures[0]);
+            glTexImage2D(GL_TEXTURE_2D, 0, mTextureFormat, mWidth, mHeight, 0, mTextureFormat, GL_UNSIGNED_BYTE, image.getPixels());
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+            mTextureID = textures[0];
+            return true;
+        }
+        else
+        {
+            LOGE("ERROR in loadTexture!");
+            return false;
+        }
     }
     else
     {
-        LOGE("ERROR in loadTexture!");
-        return false;
+        bind();
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, mWidth, mHeight, mTextureFormat, GL_UNSIGNED_BYTE, image.getPixels());
+        return true;
     }
 }
 
