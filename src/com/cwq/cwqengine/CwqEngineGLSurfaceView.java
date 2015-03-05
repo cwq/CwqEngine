c368b682fa4ac0ee98a646547b8c63ee1b10f688
cwq <common.cwq@gmail.com>
2015-03-05 16:47:55 +0800

    GLSurfaceView onPause should affter handleOnPause; onResume do not work

--- src/com/cwq/cwqengine/CwqEngineGLSurfaceView.java
+++ src/com/cwq/cwqengine/CwqEngineGLSurfaceView.java
@@ -68,6 +68,7 @@ public class CwqEngineGLSurfaceView extends GLSurfaceView {
     @Override
     public void onResume() {
         super.onResume();
+        this.setRenderMode(RENDERMODE_CONTINUOUSLY);
         queueEvent(new Runnable() {
             @Override
             public void run() {
@@ -78,13 +79,14 @@ public class CwqEngineGLSurfaceView extends GLSurfaceView {
     
     @Override
     public void onPause() {
-        super.onPause();
         queueEvent(new Runnable() {
             @Override
             public void run() {
                 mCwqEngineRenderer.handleOnPause();
             }
         });
+        this.setRenderMode(RENDERMODE_WHEN_DIRTY);
+        super.onPause();
     }
     
     @Override
