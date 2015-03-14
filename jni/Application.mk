b6dd45e4b0b176f180864d1d7fae5a716c66e030
cwq <common.cwq@gmail.com>
2015-03-14 17:01:19 +0800

    modify Application.mk  change use gnustl

--- jni/Application.mk
+++ jni/Application.mk
@@ -2,12 +2,12 @@ APP_PLATFORM := android-9
 APP_OPTIM := release
 APP_ABI := armeabi-v7a
 
-# APP_STL := stlport_shared  --> does not seem to contain C++11 features
-#APP_STL := gnustl_shared
-APP_STL := c++_static
+#APP_STL := c++_static
 #NDK_TOOLCHAIN_VERSION=clang
 
-# 4.8 至少才能使用c++11
+#APP_STL := gnustl_shared
+APP_STL := gnustl_static
+#at least 4.8 can use c++11
 NDK_TOOLCHAIN_VERSION := 4.8
 
 APP_CPPFLAGS := -std=c++11
